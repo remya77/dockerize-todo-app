4651c7de64d047d121a7b36b348d32c471113f37
Remya Ramesan <remya.ramesan@ingka.ikea.com>
2024-10-11 17:42:29 +0200

    all updates

--- starter_todo_app/frontend/src/App.js
+++ starter_todo_app/frontend/src/App.js
@@ -29,7 +29,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React app
         </a>
       </header>
     </div>
